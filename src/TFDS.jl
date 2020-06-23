20cb4596fae9fb030de124e70695105fd87f23ed
us <rahmetsaritekin@gmail.com>
2020-06-23 22:20:40 +0300

    Version 0.3

--- src/TFDS.jl
+++ src/TFDS.jl
@@ -3,28 +3,30 @@ module TFDS
 using PyCall
 
 const tfds = PyNULL()
-export tfds,
-    Split,
-    load,
+export core,
     as_numpy,
+    decode,
+    download,
+    features,
+    units,
+    GenerateMode,
     builder,
+    builder_cls,
+    list_builders,
+    load,
+    ReadConfig,
+    Split,
+    testing,
     disable_progress_bar,
     is_dataset_on_gcs,
-    list_builders,
-    ReadConfig
+    show_examples,
+    visualization,
+    __version__
 
 
 function __init__()
     copy!(tfds, pyimport_conda("tensorflow_datasets", "tensorflow_datasets"))
 end
-
-function list_builders()
-    tfds.list_builders()
-end
-
-# function load(args...; kwargs...)
-#     tfds.as_numpy(tfds.load(args..., batch_size=-1; kwargs...))
-# end
 
 macro delegate(f_list...)
     blocks = Expr(:block)
@@ -39,6 +41,24 @@ macro delegate(f_list...)
     blocks
 end
 
-@delegate load Split as_numpy builder disable_progress_bar is_dataset_on_gcs list_builders ReadConfig
+@delegate core
+    as_numpy
+    decode
+    download
+    features
+    units
+    GenerateMode
+    builder
+    builder_cls
+    list_builders
+    load
+    ReadConfig
+    Split
+    testing
+    disable_progress_bar
+    is_dataset_on_gcs
+    show_examples
+    visualization
+    __version__
 
 end # module
